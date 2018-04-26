c600dad31263612290d5828e0f7e708671aa5692
frusteron <frusteron88@gmail.com>
2018-04-26 17:00:35 +0200

    incompleti

---
+++ ShopOnline2/src/it/accenture/controller/Acquista.java
@@ -0,0 +1,25 @@
+package it.accenture.controller;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+public class Acquista extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+
+}
